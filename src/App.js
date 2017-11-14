431eca0e351eb5deea597237b273b04cf35d9c9a
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-11-14 18:45:03 +0530

    app js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import SearchComponent from '../src/components/SearchComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchComponent/>
+      </div>
+    );
+  }
+}
+
+export default App;
