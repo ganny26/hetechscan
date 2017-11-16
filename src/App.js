1c61fe9744a140b74939804eea43e186a1863252
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-11-16 18:45:09 +0530

    added pagination component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 import './App.css';
 import SearchComponent from '../src/components/SearchComponent';
-
+import PaginationComponent from '../src/components/PaginationComponent';
 class App extends Component {
   render() {
     return (
       <div>
         <SearchComponent/>
+        <PaginationComponent/>
       </div>
     );
   }
