6d6ed5e7ab50fd463da9bcb7bdbc6d4b64383a42
selvaganeshr <selvaganeshrajam@gmail.com>
2017-12-12 08:06:52 +0530

    checkbox component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchComponent from '../src/components/SearchComponent';
 import PaginationComponent from '../src/components/PaginationComponent';
+import CheckboxComponent from '../src/components/CheckboxComponent';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <SearchComponent/>
         <PaginationComponent/>
+        <CheckboxComponent value="Option 1"/>
+        <CheckboxComponent value="Option 2" />
       </div>
     );
   }
