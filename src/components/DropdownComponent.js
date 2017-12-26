0a61c2687c0ba64de4481b20e1aca149c4656415
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-12-26 14:35:36 +0530

    ddcomponent added

--- src/components/DropdownComponent.js
+++ src/components/DropdownComponent.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
+let ddList = ['Java', 'Node JS', 'Python', 'Shell', 'Bash'];
 
+const DropdownItem = (props) => {
+    const itemList = props.itemList;
+    const listItems = itemList.map((value, index,array) =>
+        <li className="dropdown-item" key={index}>
+            {value}
+        </li>
+    );
+    return (<ul>{listItems}</ul>);
+}
 
+let voices = window.speechSynthesis;
 class DropdownComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "option": []
+        }
 
+    }
+    componentWillMount() {
+        this.setState({
+            "option":this.props.options
+        })
+    }
     render() {
         return (
-            <div class="dropdown show">
-                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown link
-                </a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
+            <div className="dropdown">
+                <button className="btn btn-primary dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton"
+                    data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+            </button>
+            <div className="dropdown-menu" >
+               <DropdownItem itemList={this.state.option}/>
+               </div>
             </div>
         )
     }
