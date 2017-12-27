586b049ab82c05bd94c6d9ae3beeb3d392a88109
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-12-27 19:03:52 +0530

    updated component recive prop

--- src/App.js
+++ src/App.js
@@ -9,22 +9,75 @@ import './App.css';
 import CaptionComponent from '../src/components/CaptionComponent';
 import SideBar from '../src/components/SideBar';
 import Main from '../src/components/Main';
+let digits = [
+  "An",
+  "inside",
+  "look",
+  "from",
+  "Team",
+  "CodePen",
+  "on",
+  "what",
+  "it's",
+  "like",
+  "running",
+  "a",
+  "web",
+  "business.",
+  "Everything",
+  "from",
+  "server",
+  "infrastructure,",
+  "to",
+  "day-to-day",
+  "operations,",
+  "to",
+  "new",
+  "feature",
+  "development."
+];
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    }
+    this.narate = this.narate.bind(this)
+
+  }
+
+
+  narate() {
+    console.log("updated");
+    this.setState({
+      value: this.state.value + 1
+    })
+  }
+
+
   render() {
     return (
       <div>
         <div className="container" style={{ marginTop: 30 }}>
           <div className="row">
             <div className="col-md-3">
+          
               <SideBar />
+              <button className="btn btn-success" onClick={this.narate.bind(this)}>Update</button>
             </div>
             <div className="col-md-8">
+         
               <Main />
             </div>
           </div>
         </div>
-        <CaptionComponent />
+      
+        <CaptionComponent narration={this.state.value} />
+      
       </div>
     );
   }
--- src/components/CaptionComponent.js
+++ src/components/CaptionComponent.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import DropdownComponent from '../components/DropdownComponent';
 import '../App.css';
 class CaptionComponent extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            "narrationText":""
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            narrationText:this.props.narration
+        })
+    }
+
+    componentWillReceiveProps(newProps) {
+        this.setState({narrationText:newProps.narration});
+    }
     render() {
         return (
             <div className="ccLayer">
                 <div className="ccText">
-                    Caption Component
+                   {this.state.narrationText}
             </div>
             </div>
         )
